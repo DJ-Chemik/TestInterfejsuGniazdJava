753118746b1504759050adcbbcd3c6dcca88897f
DJ Chemik <dj-chemik@wp.pl>
2019-11-28 23:03:13 +0100

    Pierwsza wersja która lekko działa

--- app/src/main/java/pl/chemik/testinterfejsugniazd/MainActivity.java
+++ app/src/main/java/pl/chemik/testinterfejsugniazd/MainActivity.java
@@ -16,37 +16,35 @@ public class MainActivity extends AppCompatActivity {
     private static final int SERVERPORT = 1234;
     private static final String SERVER_IP = "192.168.0.108";
     private Socket socket; // Utworzenie gniazda
-    private ClientThread watekObslugiSieci;
+    public Thread watekObslugiSieci;
 
     private ArrayList<String> indeksy = new ArrayList<>(Arrays.asList("136809", "132336", "131313"));
     private TextView tv_wynik;
 
 
-    public void sendIndex1(View view) throws IOException {
-        watekObslugiSieci.send(indeksy.get(0));
-        System.out.println(watekObslugiSieci.receive());
+    public void sendIndex1(View v){
+        watekObslugiSieci = new Thread(new ClientThread());
+        watekObslugiSieci.start();
+        watekObslugiSieci.interrupt();
     }
 
     public void sendIndex2(View v) {
-        watekObslugiSieci.send(indeksy.get(1));
-        System.out.println(watekObslugiSieci.receive());
+
     }
 
     public void sendIndex3(View v) {
-        watekObslugiSieci.send(indeksy.get(2));
-        System.out.println(watekObslugiSieci.receive());
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        watekObslugiSieci = new ClientThread();
-        watekObslugiSieci.start();
+
 
     }
 
-    class ClientThread extends Thread {
+    class ClientThread implements Runnable{
 
         private byte[] buffer = new byte[1024];
 
@@ -57,56 +55,50 @@ public class MainActivity extends AppCompatActivity {
 
                 InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
                 socket = new Socket(serverAddr, SERVERPORT);
-//                    socket.close();
+                OutputStream outputstream = null;
+                try {
+                    outputstream = socket.getOutputStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                PrintWriter out = new PrintWriter(outputstream);
+                out.write(indeksy.get(0));
+                out.flush();
 
+
+                InputStream in = null;
+                String output = "Brak danych... :-(\n";
+
+                try {
+                    in = socket.getInputStream();
+
+                    int read;
+                    while ((read = in.read(buffer)) != -1) {
+                        output = new String(buffer, 0, read);
+                        System.out.print(output);
+                        System.out.flush();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                socket.close();
             } catch (UnknownHostException e1) {
                 e1.printStackTrace();
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
 
+
         }
 
         public void send(int wartoscZListyIndeksow) {
-            OutputStream outputstream = null;
-            try {
-                outputstream = socket.getOutputStream();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            PrintWriter out = new PrintWriter(outputstream);
-            out.write(indeksy.get(wartoscZListyIndeksow));
-            out.flush();
+
         }
 
-        public void send(String indeks) {
-            OutputStream outputstream = null;
-            try {
-                outputstream = socket.getOutputStream();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            PrintWriter out = new PrintWriter(outputstream);
-            out.write(indeks);
-            out.flush();
-        }
 
         public String receive() {
-            InputStream in = null;
-            String output = "Brak danych... :-(\n";
-
-            try {
-                in = socket.getInputStream();
-
-                int read;
-                while ((read = in.read(buffer)) != -1) {
-                    output = new String(buffer, 0, read);
-                    System.out.print(output);
-                    System.out.flush();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            String output="";
             return output;
         }
 
